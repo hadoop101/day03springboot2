ba9e1381059036fe6ce916dbd93f33ca03c7b9c0
hadoop101 <hadoop101@126.com>
2020-06-21 21:13:53 +0800

    整合Junit

---
+++ m3_springboot_devtools/src/main/java/com/example/m3_springboot_devtools/bean/Account.java
@@ -0,0 +1,49 @@
+package com.example.m3_springboot_devtools.bean;
+
+public class Account {
+    private Long id;
+    private String account;
+    private Double money;
+
+    public Account() {
+    }
+
+    public Account(Long id, String account, Double money) {
+        this.id = id;
+        this.account = account;
+        this.money = money;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", account='" + account + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
---
+++ m3_springboot_devtools/src/test/java/com/example/m3_springboot_devtools/M3SpringbootDevtoolsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.m3_springboot_devtools;
+
+import com.example.m3_springboot_devtools.bean.Account;
+import com.example.m3_springboot_devtools.service.AccountService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3SpringbootDevtoolsApplicationTests {
+
+    @Autowired
+    AccountService accountService = null;
+    @Test
+    void test01Service() {
+        //AccountService acocuntService = new AccountServiceImpl();
+        Account account = new Account(1001L,"jack",33D);
+        accountService.saveAccount(account);
+
+    }
+
+}
